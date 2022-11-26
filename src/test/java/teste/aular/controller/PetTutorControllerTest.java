c933327a05b6b9ca1a9d12c6734d5d05071d6bbb
WallisonGalvao <wallison08@gmail.com>
2022-11-26 13:23:07 -0300

    feat: RestController annotation required

--- src/test/java/teste/aular/controller/PetTutorControllerTest.java
+++ src/test/java/teste/aular/controller/PetTutorControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import teste.aular.domain.contract.AddressRepository;
 import teste.aular.domain.contract.PetRepository;
 import teste.aular.domain.contract.PetTutorRepository;
@@ -32,6 +33,13 @@ class PetTutorControllerTest {
     void mustHaveRequestMappingAnnotation() {
         assertTrue(controller.getClass().isAnnotationPresent(RequestMapping.class),
                 "@RequestMapping annotation not found");
+    }
+
+    @Test
+    @DisplayName("Class must have @RestController annotation")
+    void mustHaveRestControllerAnnotation() {
+        assertTrue(controller.getClass().isAnnotationPresent(RestController.class),
+                "@RestController annotation not found");
     }
 
 }
