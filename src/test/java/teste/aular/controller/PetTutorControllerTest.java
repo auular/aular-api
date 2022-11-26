ae4d66b81ef5cf7fe4797fa16124ee93ac2eb537
WallisonGalvao <wallison08@gmail.com>
2022-11-26 13:19:37 -0300

    feat: @RequestMapping annotation required

---
+++ src/test/java/teste/aular/controller/PetTutorControllerTest.java
@@ -0,0 +1,37 @@
+package teste.aular.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import teste.aular.domain.contract.AddressRepository;
+import teste.aular.domain.contract.PetRepository;
+import teste.aular.domain.contract.PetTutorRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = PetTutorController.class)
+class PetTutorControllerTest {
+
+    @MockBean
+    private PetTutorRepository repository;
+
+    @MockBean
+    private PetRepository petRepository;
+
+    @MockBean
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private PetTutorController controller;
+
+    @Test
+    @DisplayName("Class must have @RequestMapping annotation")
+    void mustHaveRequestMappingAnnotation() {
+        assertTrue(controller.getClass().isAnnotationPresent(RequestMapping.class),
+                "@RequestMapping annotation not found");
+    }
+
+}
