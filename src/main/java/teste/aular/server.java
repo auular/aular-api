078b8702a166dc5fab3c1742a4a18a68f26370a0
gabrielsouzaexe <gabrielsouza628@gmail.com>
2023-03-05 18:58:05 -0300

    fix: test deploy

---
+++ src/main/java/teste/aular/server.java
@@ -0,0 +1,14 @@
+package teste.aular;
+
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class server implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        factory.setPort(80);
+    }
+}
